19e2e57a8522ecaa26a6f75d3a727a25bc22be84
Jianfeng Yang <jianfeng.yang0120@gmail.com>
2018-03-28 14:39:10 -0500

    update these methods to get json from server

--- BitcoinTicker/ViewController.swift
+++ BitcoinTicker/ViewController.swift
@@ -63,24 +63,24 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
 //    //MARK: - Networking
 //    /***************************************************************/
 //    
-//    func getWeatherData(url: String, parameters: [String : String]) {
-//        
-//        Alamofire.request(url, method: .get, parameters: parameters)
-//            .responseJSON { response in
-//                if response.result.isSuccess {
-//
-//                    print("Sucess! Got the weather data")
-//                    let weatherJSON : JSON = JSON(response.result.value!)
-//
-//                    self.updateWeatherData(json: weatherJSON)
-//
-//                } else {
-//                    print("Error: \(String(describing: response.result.error))")
-//                    self.bitcoinPriceLabel.text = "Connection Issues"
-//                }
-//            }
-//
-//    }
+    func getCurrencyData(url: String) {
+        
+        Alamofire.request(url, method: .get)
+            .responseJSON { response in
+                if response.result.isSuccess {
+
+                    print("Sucess! Got the currency data")
+                    let currencyJSON : JSON = JSON(response.result.value!)
+
+                    self.updateCurrencyData(json: currencyJSON)
+
+                } else {
+                    print("Error: \(String(describing: response.result.error))")
+                    self.bitcoinPriceLabel.text = "Connection Issues"
+                }
+            }
+
+    }
 //
 //    
 //    
@@ -89,19 +89,16 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
 //    //MARK: - JSON Parsing
 //    /***************************************************************/
 //    
-//    func updateWeatherData(json : JSON) {
-//        
-//        if let tempResult = json["main"]["temp"].double {
-//        
-//        weatherData.temperature = Int(round(tempResult!) - 273.15)
-//        weatherData.city = json["name"].stringValue
-//        weatherData.condition = json["weather"][0]["id"].intValue
-//        weatherData.weatherIconName =    weatherData.updateWeatherIcon(condition: weatherData.condition)
-//        }
-//        
-//        updateUIWithWeatherData()
-//    }
-//    
+    func updateCurrencyData(json : JSON) {
+        
+        if let tempResult = json["averages"]["day"].double {
+            self.bitcoinPriceLabel.text = currentCurrencySign + String(tempResult)
+        }
+        else {
+            self.bitcoinPriceLabel.text = "Price unavailable"
+        }
+    }
+    
 
 
 
